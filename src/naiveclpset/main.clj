0d1a26a70ed50fda383131978bf2902e24a6d238
Alex Ati <alejandro@atienzaramos.com>
2021-12-07 16:46:01 +0100

    Added set-map unification

--- src/naiveclpset/main.clj
+++ src/naiveclpset/main.clj
@@ -9,15 +9,36 @@
   (walk-term [v f] (with-meta (set (walk-term (seq v) f)) (meta v))))
 
 (extend-protocol IUnifyTerms
+  clojure.lang.IPersistentMap
+  (unify-terms [u v s] ; adapted from the original definition...
+    (cond
+      (instance? clojure.core.logic.protocols.IUnifyWithRecord v)
+      (unify-with-record v u s)
+
+      (map? v)
+      (unify-with-map* u v s)
+
+      (set? v) ; ...to add the option to unify with a set...
+      (unify-terms v u s) ; ...which is handled below for the symmetric case
+
+      :else nil))
+
   clojure.lang.IPersistentSet
   (unify-terms [u v s]
-    (when (set? v)
+    (cond
+      (map? v)
+      (unify-terms u (set v) s) ; a map is just a set of key-value vectors
+
+      (set? v)
       (let [u (seq u)
             v (seq v)]
         (reduce #(mplus % (-inc %2))
-                (for [p (permutations u)] (unify s p v)))))))
+                (for [p (permutations u)] (unify s p v))))
+
+      :else nil)))
 
 (comment
+  ;; original author tests
   (run* [q] (== q []))
   (run* [q] (== q #{}))
   (run* [q] (== q [1 2 3]))
@@ -31,4 +52,9 @@
   (run* [q] (== #{1 2 [3 q]} #{1 2 [3 4]}))
   (run* [q] (== #{1 2 #{3 q}} #{1 2 #{3 4}})) ; fails!
   (run* [q] (== #{ #{ #{q} :bar} :baz}  #{:baz #{:bar #{:foo}}}))
+
+  ;; a set of kv vectors should unify with a map
+  (run* [q] (== #{[:a 1]} #{[:a q]}))
+  (run* [q] (== #{[:a 1]} {:a q}))
+  (run* [q] (== {:a 1} #{[:a q]}))
 )
